5da871abb3d17c65cd1b70113a07ad5d47484fa7
dominikims <76172478+dominikims@users.noreply.github.com>
2022-01-29 11:53:38 +0100

    Temperature classes
    made temperature classes

--- Assets/DominikAssets/Scripts/PlayerTemperature.cs
+++ Assets/DominikAssets/Scripts/PlayerTemperature.cs
@@ -7,13 +7,67 @@ public class PlayerTemperature : MonoBehaviour
     public float temperature;
 
 
-    public enum TemperatureType {Normal, Hot, VeryHot, Cold, VeryCold };
+    public enum TemperatureType {Normal, Hot, VeryHot, DeadlyHot, Cold, VeryCold, DeadlyCold };
+
+
+    //Define ranges of TemperatureTypes
+    public float maxNormalTemp;
+
+    public float maxHotTemp;
+
+    public float deadlyHotTemp;
+
 
 
     //change temperature by value "change"
     public void ChangeTemperature(float change)
     {
         temperature += change;
+    }
+
+
+    public TemperatureType GetTemperatureType()
+    {
+
+        if(Mathf.Abs(temperature) < maxNormalTemp)
+        {
+            return TemperatureType.Normal;
+        }
+        else if(temperature > 0)
+        {
+            //Postiive (hot) temperatures
+            if (Mathf.Abs(temperature) < maxHotTemp)
+            {
+                return TemperatureType.Hot;
+            }
+            else if (Mathf.Abs(temperature) < deadlyHotTemp)
+            {
+                return TemperatureType.VeryHot;
+            }
+            else if (Mathf.Abs(temperature) >= deadlyHotTemp)
+            {
+                return TemperatureType.DeadlyHot;
+            }
+        }
+        else if (temperature < 0)
+        {
+            //Negative (cold) temperatures
+            if (Mathf.Abs(temperature) < maxHotTemp)
+            {
+                return TemperatureType.Cold;
+            }
+            else if (Mathf.Abs(temperature) < deadlyHotTemp)
+            {
+                return TemperatureType.VeryCold;
+            }
+            else if (Mathf.Abs(temperature) >= deadlyHotTemp)
+            {
+                return TemperatureType.DeadlyCold;
+            }
+        }
+
+        return TemperatureType.Normal;
+
     }
 
 
