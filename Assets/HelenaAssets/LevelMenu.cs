dfede4a0936d9c6b9415335845bd6b4630b138f9
HelenaAdam <helena.adam14@gmail.com>
2022-01-30 13:00:12 +0100

    Menu changes

---
+++ Assets/HelenaAssets/LevelMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+        Debug.Log("Load Level 1");
+    }
+
+    public void LoadLevel2()
+    {
+        //SceneManager.LoadScene("Level2");
+        Debug.Log("Load Level 2");
+    }
+
+    public void LoadLevel3()
+    {
+        //SceneManager.LoadScene("Level3");
+        Debug.Log("Load Level 3");
+    }
+
+    public void ComingSoon()
+    {
+        Debug.Log("Coming soon...");
+    }
+}
